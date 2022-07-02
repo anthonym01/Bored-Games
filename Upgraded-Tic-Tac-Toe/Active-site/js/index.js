3a59a19af7403513da50b5ea1918825963a85593
Samuel A. Matheson <samuelmatheson15@gmail.com>
2022-07-02 16:06:45 -0500

    consolodation

---
+++ Upgraded-Tic-Tac-Toe/Active-site/js/index.js
@@ -0,0 +1,68 @@
+
+//strip variables from links
+function get_url_variables(url) {//Gets url variables as an object
+    if (url == undefined) { url = window.location.href }
+    //Yoinked from
+    //https://gomakethings.com/getting-all-query-string-values-from-a-url-with-vanilla-js/
+    var params = {};
+    var parser = document.createElement('a');
+    parser.href = url;
+    var query = parser.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        params[pair[0]] = decodeURIComponent(pair[1]);
+    }
+    return params;
+    //returns Object { "": "undefined" } if empty
+    //Call with var this_url = getParams(window.location.href);
+}
+
+
+function linkify(text) {//Link regex :{ make brain go zapp
+    var urlRegex = /((?:(http|https|Http|Https|rtsp|Rtsp):\/\/(?:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,64}(?:\:(?:[a-zA-Z0-9\$\-\_\.\+\!\*\'\(\)\,\;\?\&\=]|(?:\%[a-fA-F0-9]{2})){1,25})?\@)?)?((?:(?:[a-zA-Z0-9][a-zA-Z0-9\-]{0,64}\.)+(?:(?:aero|arpa|asia|a[cdefgilmnoqrstuwxz])|(?:biz|b[abdefghijmnorstvwyz])|(?:cat|com|coop|c[acdfghiklmnoruvxyz])|d[ejkmoz]|(?:edu|e[cegrstu])|f[ijkmor]|(?:gov|g[abdefghilmnpqrstuwy])|h[kmnrtu]|(?:info|int|i[delmnoqrst])|(?:jobs|j[emop])|k[eghimnrwyz]|l[abcikrstuvy]|(?:mil|mobi|museum|m[acdghklmnopqrstuvwxyz])|(?:name|net|n[acefgilopruz])|(?:org|om)|(?:pro|p[aefghklmnrstwy])|qa|r[eouw]|s[abcdeghijklmnortuvyz]|(?:tel|travel|t[cdfghjklmnoprtvwz])|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw]))|(?:(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\.(?:25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[0-9])))(?:\:\d{1,5})?)(\/(?:(?:[a-zA-Z0-9\;\/\?\:\@\&\=\#\~\-\.\+\!\*\'\(\)\,\_])|(?:\%[a-fA-F0-9]{2}))*)?(?:\b|$)/gi;/*/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig*/;
+    return text.replace(urlRegex, function (url) {//returns text with imbeded html links
+        return '<a href="' + url + '">' + url + '</a>';
+    });
+}
+
+
+// post and get functions
+async function request(what) {//make a request to server for data
+
+    //what ="/get/test"; //example
+
+    try {
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function () {//wait for and handle response
+            if (this.readyState == 4 && this.status == 200) {
+                console.log('Server replied with: ', this.responseText, ' In response: ', this.response)
+                return this.responseText
+            }
+        };
+
+        xhttp.open("GET", what, true);//get request
+        xhttp.send();
+    } catch (err) {
+        console.warn('xhttp request failed ', err);
+    }
+
+}
+
+async function post(what, where) {//post data to server
+    var xhttp = new XMLHttpRequest();
+
+    //example
+    //what = {stuff:"test sending stuff",numbrero:8}
+    //where = "/post/test"
+
+    xhttp.onreadystatechange = function () {//wait for and handle response
+        if (this.readyState == 4 && this.status == 200) {
+            console.log('Server replied with: ', this.responseText, ' In response: ', this.response)
+        }
+    };
+
+    xhttp.open("POST", where, true);
+    xhttp.send(JSON.stringify(what));
+}
---
+++ Upgraded-Tic-Tac-Toe/README.md
@@ -0,0 +1,8 @@
+# Upgraded-Tic-Tac-Toe
+To be built later when i have time based on
+
+<video src="https://i.imgur.com/EhKRbfa.mp4" autoplay controls>
+  
+![example](https://i.imgur.com/EhKRbfa.mp4)
+
+https://www.reddit.com/r/awesome/comments/r7s9hh/upgraded_tic_tac_toe/
